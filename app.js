724a6ce4898b15ca1be7ee080e84531461c401ef
Anudeep Tadikamalla <anudeept2001@gmail.com>
2026-05-20 23:14:15 +0530

    Add AR video handler and debug overlay
    Register an A-Frame component (video-handler) to play/pause the #ar-video element on targetFound/targetLost events. Add a floating debug log overlay and override console.log to surface status messages in-page. Link external stylesheet and include cache-busting query params for assets and app.js to ensure updates are picked up. Minor scene attribute updates (image target src versioning) and removal of inline body styles.

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("AR Project Started");
+AFRAME.registerComponent('video-handler', {
+    init: function () {
+        console.log("Status: AR System Ready"); // Debugger will show this first
+        
+        const video = document.querySelector("#ar-video");
+
+        this.el.addEventListener('targetFound', event => {
+            console.log("Target Found: Playing Video"); // Dash will update to this
+            video.play();
+        });
+
+        this.el.addEventListener('targetLost', event => {
+            console.log("Target Lost: Video Paused"); // Dash will update to this
+            video.pause();
+        });
+    }
+});
